7b6baeb0e87544d3279d0ad9ee75b3474ca075ba
Florian-R <florian.r.dev@gmail.com>
2016-11-16 14:53:32 +0100

    Node

--- boot.sh
+++ boot.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo 'CUSTER'
+
+echo '1-Update all'
+sudo apt-get update && sudo apt-get upgrade -y
+
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+) && . "$NVM_DIR/nvm.sh"
+
+echo '3-Installing node'
+nvm install node
+
+echo '4-Upgrading npm to latest'
+npm install -g npm
